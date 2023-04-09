9ebdcf92c1a8b54869e4d4410044782e6a17806a
Kirill <shadow986@mail.ru>
2023-04-09 23:43:28 +0300

    Заполнение исходного массива

--- Program.cs
+++ Program.cs
@@ -1,4 +1,10 @@
 ﻿// сформировать из исходного массива массив строк, длина которых меньше 3-х символов 
 Console.WriteLine("Введите кол-во элементов в массиве:");
 int m = Convert.ToInt32(Console.ReadLine());
+arstring = new string[m];
+for (int count1 = 0; count1 < m; count1++)
+{
+    arstring[count1] = Console.ReadLine();
+}
+
 
