333461a418915a699e4133d091a092d4c47f0545
Kirill <shadow986@mail.ru>
2023-04-09 23:50:25 +0300

    Формирование нового масива

--- Program.cs
+++ Program.cs
@@ -6,5 +6,15 @@ for (int count1 = 0; count1 < m; count1++)
 {
     arstring[count1] = Console.ReadLine();
 }
-
-
+int count2 = 0;
+arstring2 = new string[m];
+for (int count1 = 0; count1 < m; count1++)
+{
+    if (arstring[count1].Length <= 3)
+        {
+            arstring2[count2] = arstring[count1];
+            Console.WriteLine(arstring2[count2]);
+            count2 = count2+1;
+        }
+}
+int n = count2 - 1;
