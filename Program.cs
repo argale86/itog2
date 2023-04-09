687bfafcfdbfc0b15b207eafec8772152949241c
Kirill <shadow986@mail.ru>
2023-04-09 23:55:57 +0300

    Вывод элементов нового массива

--- Program.cs
+++ Program.cs
@@ -1,13 +1,16 @@
 ﻿// сформировать из исходного массива массив строк, длина которых меньше 3-х символов 
 Console.WriteLine("Введите кол-во элементов в массиве:");
 int m = Convert.ToInt32(Console.ReadLine());
-arstring = new string[m];
+string [] arstring = new string[m];
+Console.WriteLine("Вводите элементы в массив:");
 for (int count1 = 0; count1 < m; count1++)
 {
     arstring[count1] = Console.ReadLine();
 }
+Console.WriteLine();
 int count2 = 0;
-arstring2 = new string[m];
+string [] arstring2 = new string[m];
+Console.WriteLine("Элементы новго массива:");
 for (int count1 = 0; count1 < m; count1++)
 {
     if (arstring[count1].Length <= 3)
